4966c6676c9637da82c13bd6268b8ee9663459a1
DanCarl857 <dancarl758@gmail.com>
2017-10-01 04:15:08 +0100

    Added basic structure of app

--- recipeapp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipeapp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../domain/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simple a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg'),
+    new Recipe('A Test Recipe', 'This is simple a test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
 
   constructor() { }
 
